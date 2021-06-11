7dba36a5e22ac5433a03d0e4c3a4734115fe43c3
souly <islam.mostafa.fahmey@gmail.com>
2021-06-11 23:49:13 +0200

    create db.helper model and db.factory model and catch

---
+++ models/db.factory.model.js
@@ -0,0 +1,24 @@
+/**
+ * function to get all records that matches the query on a db model
+ * @param  {Mongoose.model} dbModel
+ * @param  {Object} queryObj
+ */
+exports.getAll = async (dbModel, queryObj) => {
+  const dbHelper = new DbHelper(dbModel, queryObj).filter();
+  return await dbHelper.query;
+};
+
+/**
+ * function to get all records that matches the query on a db model with response code
+ * @param  {Mongoose.model} dbModel
+ */
+exports.getAllRes = (dbModel) =>
+  helperFunctions.catchAsync(async (req, res, next) => {
+    const docs = await this.getAll(dbModel, req.validatedQueryObj);
+
+    res.status(200).json({
+      status: "success",
+      results: docs.length,
+      data: docs,
+    });
+  });
---
+++ utils/db.helper.js
@@ -0,0 +1,22 @@
+/**
+ *  in order to limit size of response & not to load all the data at the same time
+ *  in a large application a pagination should be addd to skip this limit
+ */
+const maxDocsLimit = process.env.MAX_DOCS_LIMIT || 20;
+
+module.exports = class DbHelper {
+  constructor(dbModel, queryObj = {}) {
+    this.model = dbModel;
+    this.queryObj = queryObj;
+    this.query = this.model.find();
+  }
+
+  /**
+   * function to filter the query with query obj
+   */
+  filter() {
+    // apply query
+    this.query = this.query.find(this.queryObj).limit(maxDocsLimit);
+    return this;
+  }
+};
